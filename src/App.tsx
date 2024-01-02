5dcfa03971bc707f64ca68a00dba3103a692ddf1
Andy Mancuso <andymancuso5@gmail.com>
2024-01-02 13:53:39 -0300

    feat(alternate colors): add btn to change row colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Users from './components/Users'
 
-function App () {
+const App = () => {
+  const [users, setUsers] = useState([])
+  const [isDefaultColor, setIsDefaultColor] = useState(false)
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUsers(res.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  const changeColors = () => {
+    setIsDefaultColor(prev => !prev)
+  }
+
   return (
-    <Users />
+    <>
+      <h1 style={{ fontSize: '4rem' }}>Users List</h1>
+
+      <header onClick={changeColors}>
+        <button>Colorear Filas</button>
+      </header>
+
+      <Users
+        users={users}
+        isDefaultColor={isDefaultColor}
+      />
+    </>
+
   )
 }
 
--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,21 +1,55 @@
-import { useEffect, useState } from 'react'
 import { type User } from '../../types'
-const Users = () => {
-  const [users, setUsers] = useState<User[]>([])
 
-  useEffect(() => {
-    fetch('https://randomuser.me/api?results=100')
-      .then(async res => await res.json())
-      .then(res => {
-        setUsers(res.results)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
+interface Props {
+  users: User[]
+  isDefaultColor: boolean
+}
+
+const Users = (props: Props) => {
+  const {
+    users,
+    isDefaultColor
+  } = props
 
   return (
-    <h1 style={{ fontSize: '4rem' }}>Users List</h1>
+    <>
+      <table style={{ width: '100%', marginTop: '40px' }}>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Last name</th>
+            <th>Country</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            users.map((user, index) => {
+              const backgroundColor = index % 2 === 0 ? '#333' : '#555'
+
+              return (
+                <tr
+                  key={user.login.uuid}
+                  style={{ backgroundColor: isDefaultColor ? backgroundColor : 'transparent' }}
+                >
+                  <td>
+                    <img src={user.picture.large} />
+                  </td>
+                  <td>{user.name.first}</td>
+                  <td>{user.name.last}</td>
+                  <td>{user.location.country}</td>
+                  <td>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </>
   )
 }
 
